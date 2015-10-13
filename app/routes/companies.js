9deb0c4ca09f95fbf569d35d3985046155a944de
Oladele <oladele@gmail.com>
2015-10-13 16:04:22 -0400

    Demonstrate action bubbling

--- app/routes/companies.js
+++ app/routes/companies.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	actions: {
+    
+    save() {
+      console.log('+--- save action in company route');
+			return false; 
+		},
+    
+    cancel() {
+      console.log('+--- cancel action in company route');
+      return false;
+    }
+  }
 });
--- app/routes/companies/new.js
+++ app/routes/companies/new.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model(){
+		return this.store.createRecord('company');
+	},
+
+	actions: {
+    
+    save() {
+      console.log('+-- save action in company new route');
+			return true; 
+		},
+    
+    cancel() {
+      console.log('+-- cancel action in company new route');
+      return true;
+    }
+  }
+
 });
