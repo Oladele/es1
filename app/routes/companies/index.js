7b8ee24ee02c316d54975ca71fd88d44fe2db799
Oladele <oladele@gmail.com>
2015-10-14 07:53:34 -0400

    Delete company

--- app/routes/companies/index.js
+++ app/routes/companies/index.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model() {
 		return this.store.findAll('company');
+	},
+	actions: {
+		delete(company){
+			company.destroyRecord();
+			return false;
+		}
 	}
+
 });
