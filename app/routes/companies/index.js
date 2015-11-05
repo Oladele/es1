51099792d75549b4c54b0238151932067ef749be
Oladele <oladele@gmail.com>
2015-11-05 12:51:23 -0500

    Attempt to reduce network API calls

--- app/routes/companies/index.js
+++ app/routes/companies/index.js
@@ -2,6 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		return this.store.findAll('company');
+		let store = this.store;
+
+		let response = this.store.query('company', 
+			{
+				include: 'network-sites'
+			}
+		).then((companies)=>{
+			companies.forEach((company)=>{
+
+				company.storedNetworkSites = store.filter('network-site',(ns)=>{
+					// return ns.get('company').get("id") == company.id;
+					return company.get('networkSites').indexOf(ns) >= 0;
+				});
+			})
+			return companies
+		})
+
+		return response;
+
 	}
 });
