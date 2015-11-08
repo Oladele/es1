118e60a5b40231b7abfd3054460fda812b51db56
Oladele <oladele@gmail.com>
2015-11-08 13:18:35 -0500

    cleanup unused var

--- app/routes/companies/index.js
+++ app/routes/companies/index.js
@@ -2,13 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		let store = this.store;
 
 		let response = this.store.query('company', 
 			{
 				include: 'network-sites'
 			}
-		)
+		);
 
 		return response;
 
