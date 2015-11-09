fb90bfd41b2e6964395f08a089e03f4740954309
Oladele <oladele@gmail.com>
2015-11-09 14:51:58 -0500

    Add network-site location and buidling to default data

--- app/mirage/scenarios/default.js
+++ app/mirage/scenarios/default.js
@@ -1,12 +1,60 @@
 export default function( server ) {
 
-  // Seed your development database using your factories. This
-  // data will not be loaded in your tests.
+	// Princeton
 
-  // server.createList('contact', 10);
-  server.createList('company', 4);
-  var company = server.create('company', {name: "DEFCON"});
-  server.createList('network-site', 2, {
-  	company: company.id
+  var princetonCompany = server.create('company', {name: "Princeton University"});
+  
+  server.create('network-site', {
+  	company: princetonCompany.id,
+  	name: "Princeton Campus",
+    address: "Princeton, NJ 08544, United States",
+    lat: 40.3467174, 
+    lng: -74.6568772
   });
+
+  // Erickson
+
+  // - Company
+  var ericksonCompany = server.create('company', {name: "Erickson Living"});
+
+    // --->Network Site
+    var charlestown = server.create('network-site', {
+    	company: ericksonCompany.id,
+    	name: "Charlestown",
+      address: "715 Maiden Choice Lane, Catonsville, MD, United States",
+      lat: 39.2690103, 
+      lng: -76.7014073
+    });
+
+      // ------>Building
+      server.create('network-site', {
+        "network-site": charlestown.id,
+        name: "Arborside",
+        lat: 39.2690564001304, lng: -76.7021008550728
+      });
+
+
+    server.create('network-site', {
+    	company: ericksonCompany.id,
+    	name: "Lantern Hill",
+      address: "603 Mountain Avenue, New Providence, NJ 07974, United States",
+      lat: 40.687177,
+      lng: -74.400954
+    });
+
+    server.create('network-site', {
+    	company: ericksonCompany.id,
+    	name: "Ashby Ponds",
+      address: "21170 Ashby Ponds Boulevard, Ashburn, VA 20147, United States",
+      lat: 39.0294136,
+      lng: -77.4549737
+    });
+
+    server.create('network-site', {
+    	company: ericksonCompany.id,
+    	name: "Greenspring Valley",
+      address: "7410 Spring Village Drive, Springfield, VA 22150, United States",
+      lat: 38.7671371, 
+      lng: -77.2032638
+    });
 }
