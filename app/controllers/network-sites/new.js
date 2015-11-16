a0e3c22c8eea68898f422b09465caa7762d1ea20
Oladele <oladele@gmail.com>
2015-11-16 15:17:51 -0800

    Create network-site button routes to new view form

--- app/controllers/network-sites/new.js
+++ app/controllers/network-sites/new.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	queryParams: ['company'],
+	queryParams: ['companyId'],
   companyId: null,
+  company: Ember.computed('companyId', function(){
+		return this.store.find('network-site', this.get('companyId'));
+	}),
 });
