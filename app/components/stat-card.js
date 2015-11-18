6a254f7d605191b187933fa703c8bfa69191baea
Oladele <oladele@gmail.com>
2015-11-18 08:36:38 -0800

    working stat-card component

--- app/components/stat-card.js
+++ app/components/stat-card.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	title: "",
+	value: 0,
+	cssSizes: "col-sm-6 col-md-3",
 });
--- tests/integration/components/stat-card-test.js
+++ tests/integration/components/stat-card-test.js
@@ -6,21 +6,21 @@ moduleForComponent('stat-card', 'Integration | Component | stat card', {
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
+  // assert.expect(2);
+  assert.expect(1);
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
   this.render(hbs`{{stat-card}}`);
-
-  assert.equal(this.$().text().trim(), '');
+  assert.equal( this.$('.statcard').length, 1 );
 
   // Template block usage:
-  this.render(hbs`
-    {{#stat-card}}
-      template block text
-    {{/stat-card}}
-  `);
+  // this.render(hbs`
+  //   {{#stat-card}}
+  //     template block text
+  //   {{/stat-card}}
+  // `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  // assert.equal(this.$().text().trim(), 'template block text');
 });
